00c20c56c3210634d76b6c32057c479ec19eeccd
kav-oficial <katrade1997@gmail.com>
2025-04-25 11:25:50 +0700

    ka updaetd

--- Assets/Script/RockPaperScissors/RPSManager.cs
+++ Assets/Script/RockPaperScissors/RPSManager.cs
@@ -5,36 +5,42 @@ using TMPro;
 public class RPSManager : MonoBehaviour
 {
     public Image imagePlayerChoice, imageComputerChoice;
-    public TextMeshProUGUI textPlayer;
-    public GameObject _RPSobject, _WinObject, _LoseObject, _DrawObject;
-    public CanvasGroup canvasGroup;
-    public Sprite Rock, Paper, Scinssor, spriteQuestion;
+    public TextMeshProUGUI textPlayer,textScoreDraw,textScoreWin,textScoreLose;
+    public GameObject _itemPrefab,_RPSobject, _WinObject, _LoseObject, _DrawObject,_buttonStartObject;
+    public Transform contentParent;
+    public CanvasGroup canvasGroup, messageCanvasGroup;
+    public Sprite Rock, Paper, Scinssor, spriteQuestion,playerSpritePrefab,computerSpritePrefab;
     private string playerHand = "";
     private string computer = "";
     private string[] RandomHand = { "Rock", "Paper", "Scinssor" };
-
+    int countDraw = 0,countWin=0,countLose=0;
     void Awake()
     {
         string playerName = PlayerPrefs.GetString("player", "NONE");
         textPlayer.text = playerName;
+        // _buttonStartObject.SetActive(false);
     }
     public void onClickSetHand(int value)
     {
         if (value == 1)
         {
-            imagePlayerChoice.sprite = Rock;
+            imagePlayerChoice.sprite  = Rock;
+            playerSpritePrefab = Rock;
             playerHand = "Rock";
         }
         if (value == 2)
         {
-            imagePlayerChoice.sprite = Paper;
+            imagePlayerChoice.sprite  = Paper;
+            playerSpritePrefab = Paper;
             playerHand = "Paper";
         }
         if (value == 3)
         {
-            imagePlayerChoice.sprite = Scinssor;
+            imagePlayerChoice.sprite  = Scinssor;
+            playerSpritePrefab = Scinssor;
             playerHand = "Scinssor";
         }
+        _buttonStartObject.SetActive(true);
     }
     public void onCLickStartGame()
     {
@@ -58,14 +64,17 @@ public class RPSManager : MonoBehaviour
 
         if (computer == "Rock")
         {
+            computerSpritePrefab = Rock;
             imageComputerChoice.sprite = Rock;
         }
         else if (computer == "Paper")
         {
+            computerSpritePrefab = Paper;
             imageComputerChoice.sprite = Paper;
         }
         else
         {
+            computerSpritePrefab = Scinssor;
             imageComputerChoice.sprite = Scinssor;
         }
 
@@ -73,6 +82,7 @@ public class RPSManager : MonoBehaviour
         if (playerHand == computer)
         {
             StartCoroutine(playerDraw());
+            countDraw++;
         }
         else if (
             (playerHand == "Rock" && computer == "Scinssor") ||
@@ -80,12 +90,35 @@ public class RPSManager : MonoBehaviour
             (playerHand == "Scinssor" && computer == "Paper")
         )
         {
-           StartCoroutine(playerWin());
+            countWin++;
+            StartCoroutine(playerWin());
+            Debug.Log("Win " + countWin);
         }
         else
         {
+            countLose++;
             StartCoroutine(playerLose());
+            Debug.Log("Lose " + countLose);
         }
+    }
+ 
+    public void createPrefabList(Sprite PlayerSprite, Sprite ComputerSprite, string textResult)
+    {
+        GameObject newItem = Instantiate(_itemPrefab, contentParent);
+
+        newItem.transform.SetSiblingIndex(0);
+
+        Image _prefabImagePlayer = newItem.transform.Find("labelPlayer/playerHistorySprite").GetComponent<Image>();
+        Image _prefabImageComputer = newItem.transform.Find("labelComputer/computerHistorySprite").GetComponent<Image>();
+        TextMeshProUGUI _prefabtextResult = newItem.transform.Find("labelComputer/textHistoryResult").GetComponent<TextMeshProUGUI>();
+
+        _prefabImagePlayer.sprite = PlayerSprite;
+        _prefabImageComputer.sprite = ComputerSprite;
+        _prefabtextResult.text = textResult;
+
+        Debug.Log("Player Sprite: " + PlayerSprite.name);
+        Debug.Log("Computer Sprite: " + ComputerSprite.name);
+        Debug.Log("Text Result: " + textResult);
     }
 
     IEnumerator playerDraw()
@@ -94,13 +127,34 @@ public class RPSManager : MonoBehaviour
         _RPSobject.SetActive(false);
         yield return new WaitForSeconds(3);
         _DrawObject.SetActive(false);
+        textScoreDraw.text = countDraw.ToString();
+        // _buttonStartObject.SetActive(false);
+
+        createPrefabList(playerSpritePrefab, computerSpritePrefab, "Draw");
+
+        // newItem.transform.localScale = Vector3.one;
     }
     IEnumerator playerWin()
     {
-        _WinObject.SetActive(true);
+        messageCanvasGroup.alpha = 1;
+        messageCanvasGroup.gameObject.SetActive(true);
+
         _RPSobject.SetActive(false);
+
+        LeanTween.value(canvasGroup.gameObject, 0f, 1f, 1f)
+            .setOnUpdate((float val) =>
+            {
+                canvasGroup.alpha = val;
+            })
+            .setEase(LeanTweenType.easeInOutQuad);
+
         yield return new WaitForSeconds(3);
         _WinObject.SetActive(false);
+        textScoreWin.text = countWin.ToString();
+        // _buttonStartObject.SetActive(false);
+
+        createPrefabList(playerSpritePrefab, computerSpritePrefab, "Win");
+         
     }
     IEnumerator playerLose()
     {
@@ -108,5 +162,8 @@ public class RPSManager : MonoBehaviour
         _RPSobject.SetActive(false);
         yield return new WaitForSeconds(3);
         _LoseObject.SetActive(false);
+        textScoreLose.text = countLose.ToString();
+        // _buttonStartObject.SetActive(false);
+         createPrefabList(playerSpritePrefab, computerSpritePrefab, "Lose");
     }
 }
