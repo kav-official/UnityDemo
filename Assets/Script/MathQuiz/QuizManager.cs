0ea4b45de1af0f67ddf09d7a3c4cd6f47802db3f
kav-oficial <katrade1997@gmail.com>
2025-04-26 16:31:22 +0700

    ka updated

--- Assets/Script/MathQuiz/QuizManager.cs
+++ Assets/Script/MathQuiz/QuizManager.cs
@@ -6,9 +6,11 @@ using System.Collections.Generic;
 
 public class QuizManager : MonoBehaviour
 {
-    public TextMeshProUGUI textTimeCountDown;
+    public TextMeshProUGUI textTimeCountDown,resultCorrect,resultMiss;
     public TMP_InputField playerAnswer;
-    public CanvasGroup canvasAlert;
+    public Transform _itemContent;
+    public GameObject _itemPrefab;
+    public CanvasGroup canvasAlert,canvasResult;
     public TextMeshProUGUI textQuizs, textQuizCounter, textplayerScore, textAlert;
     public Button nextButton;
     private Coroutine countDownCoroutine;
@@ -42,7 +44,7 @@ public class QuizManager : MonoBehaviour
     }
     void Update()
     {
-        if (currentIndex == 9 || timeRemaining <= 0)
+        if (currentIndex == 10 || timeRemaining <= 0)
         {return;}
         else
         {
@@ -53,7 +55,6 @@ public class QuizManager : MonoBehaviour
                 playerAnswer.text = "";
             }
         }
-        
     }
     IEnumerator StartCountDown()
     {
@@ -64,9 +65,9 @@ public class QuizManager : MonoBehaviour
             timeRemaining -= Time.deltaTime;
             yield return null;
         }
-
         nextButton.interactable = false;
         StartCoroutine(showAlertMessage());
+        canvasResult.alpha = 1;
         textAlert.text = "Time's Over";
     }
     void showQuiz()
@@ -84,6 +85,9 @@ public class QuizManager : MonoBehaviour
 
     void onClickNextQuiz()
     {
+        int correctCore = 0;
+        int missScore = 0;
+
         if (currentIndex < quizs.Count - 1)
         {
             if (countDownCoroutine != null)
@@ -91,51 +95,160 @@ public class QuizManager : MonoBehaviour
                 StopCoroutine(countDownCoroutine);
             }
             countDownCoroutine = StartCoroutine(StartCountDown());
-            
+
             if (int.TryParse(playerAnswer.text, out playerAnswerInput))
             {
                 playerAnswerInput = playerAnswerInput;
             }
 
-            if (currentIndex == 0 && playerAnswerInput == 2)
+            if (currentIndex == 0)
             {
-                playerScore = playerScore + 1;
+                if (playerAnswerInput == 2)
+                {
+                    playerScore = playerScore + 1;
+                    correctCore = correctCore + 1;
+                    createPrefabList("1 + 1", playerAnswerInput, currentIndex + 1, "green");
+                }
+                else
+                {
+                    missScore = missScore + 1;
+                    createPrefabList("1 + 1", playerAnswerInput, currentIndex + 1, "red");
+                }
+
             }
-            else if (currentIndex == 1 && playerAnswerInput == 5)
+            else if (currentIndex == 1)
             {
-                playerScore = playerScore + 1;
+                if (playerAnswerInput == 5)
+                {
+                    playerScore = playerScore + 1;
+                    correctCore = correctCore + 1;
+                    createPrefabList("2 + 3", playerAnswerInput, currentIndex + 1, "green");
+                }
+                else
+                {
+                    missScore = missScore + 1;
+                    createPrefabList("2 + 3", playerAnswerInput, currentIndex + 1, "red");
+                }
+
             }
-            else if (currentIndex == 2 && playerAnswerInput == 3)
+            else if (currentIndex == 2)
             {
-                playerScore = playerScore + 1;
+                if (playerAnswerInput == 3)
+                {
+                    playerScore = playerScore + 1;
+                    correctCore = correctCore + 1;
+                    createPrefabList("5 - 2", playerAnswerInput, currentIndex + 1, "green");
+                }
+                else
+                {
+                    missScore = missScore + 1;
+                    createPrefabList("5 - 2", playerAnswerInput, currentIndex + 1, "red");
+                }
+
             }
-            else if (currentIndex == 3 && playerAnswerInput == 9)
+
+            else if (currentIndex == 3)
             {
-                playerScore = playerScore + 1;
+                if (playerAnswerInput == 9)
+                {
+                    playerScore = playerScore + 1;
+                    correctCore = correctCore + 1;
+                    createPrefabList("3 + 6", playerAnswerInput, currentIndex + 1, "green");
+                }
+                else
+                {
+                    missScore = missScore + 1;
+                    createPrefabList("3 + 6", playerAnswerInput, currentIndex + 1, "red");
+                }
+
             }
-            else if (currentIndex == 4 && playerAnswerInput == 3)
+            else if (currentIndex == 4)
             {
-                playerScore = playerScore + 1;
+                if (playerAnswerInput == 3)
+                {
+                    playerScore = playerScore + 1;
+                    correctCore = correctCore + 1;
+                    createPrefabList("7 - 4", playerAnswerInput, currentIndex + 1, "green");
+                }
+                else
+                {
+                    missScore = missScore + 1;
+                    createPrefabList("7 - 4", playerAnswerInput, currentIndex + 1, "red");
+                }
+
             }
-            else if (currentIndex == 5 && playerAnswerInput == 9)
+            else if (currentIndex == 5)
             {
-                playerScore = playerScore + 1;
+                if (playerAnswerInput == 9)
+                {
+                    playerScore = playerScore + 1;
+                    correctCore = correctCore + 1;
+                    createPrefabList("10 - 1", playerAnswerInput, currentIndex + 1, "green");
+                }
+                else
+                {
+                    missScore = missScore + 1;
+                    createPrefabList("10 - 1", playerAnswerInput, currentIndex + 1, "red");
+                }
+
             }
-            else if (currentIndex == 6 && playerAnswerInput == 6)
+            else if (currentIndex == 6)
             {
-                playerScore = playerScore + 1;
+                if (playerAnswerInput == 6)
+                {
+                    playerScore = playerScore + 1;
+                    correctCore = correctCore + 1;
+                    createPrefabList("2 * 3", playerAnswerInput, currentIndex + 1, "green");
+                }
+                else
+                {
+                    missScore = missScore + 1;
+                    createPrefabList("2 * 3", playerAnswerInput, currentIndex + 1, "red");
+                }
             }
-            else if (currentIndex == 7 && playerAnswerInput == 3)
+            else if (currentIndex == 7)
             {
-                playerScore = playerScore + 1;
+                if (playerAnswerInput == 3)
+                {
+                    playerScore = playerScore + 1;
+                    correctCore = correctCore + 1;
+                    createPrefabList("6 / 2", playerAnswerInput, currentIndex + 1, "green");
+                }
+                else
+                {
+                    missScore = missScore + 1;
+                    createPrefabList("6 / 2", playerAnswerInput, currentIndex + 1, "red");
+                }
+
             }
-            else if (currentIndex == 8 && playerAnswerInput == 10)
+            else if (currentIndex == 8)
             {
-                playerScore = playerScore + 1;
+                if (playerAnswerInput == 10)
+                {
+                    playerScore = playerScore + 1;
+                    correctCore = correctCore + 1;
+                    createPrefabList("8 + 2", playerAnswerInput, currentIndex + 1, "green");
+                }
+                else
+                {
+                    missScore = missScore + 1;
+                    createPrefabList("8 + 2", playerAnswerInput, currentIndex + 1, "red");
+                }
+
             }
-            else if (currentIndex == 9 && playerAnswerInput == 7)
+            else if (currentIndex == 9)
             {
-                playerScore = playerScore + 1;
+                if (playerAnswerInput == 7)
+                {
+                    playerScore = playerScore + 1;
+                    correctCore = correctCore + 1;
+                    createPrefabList("9 - 3", playerAnswerInput, currentIndex + 1, "green");
+                }
+                else
+                {
+                    missScore = missScore + 1;
+                    createPrefabList("9 - 3", playerAnswerInput, currentIndex + 1, "red");
+                }
             }
 
             currentIndex++;
@@ -143,6 +256,8 @@ public class QuizManager : MonoBehaviour
             showQuiz();
             textQuizCounter.text = quizCounter.ToString();
             textplayerScore.text = playerScore.ToString();
+            resultCorrect.text   = correctCore.ToString();
+            resultMiss.text      = missScore.ToString();
         }
         else
         {
@@ -150,7 +265,29 @@ public class QuizManager : MonoBehaviour
             textAlert.text = "Game Over";
             textQuizs.text = "Game Over";
             playerAnswer.text = "";
+            canvasResult.alpha = 1;
             nextButton.interactable = false;
+        }
+    }
+
+
+    public void createPrefabList(string quiz, int answer, int index, string colors)
+    {
+        GameObject newItem = Instantiate(_itemPrefab, _itemContent);
+
+        Image _background = newItem.transform.Find("Background").GetComponent<Image>();
+        TextMeshProUGUI _alertQuiz = newItem.transform.Find("LabelQuiz/textQuiz").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI _alertAnswer = newItem.transform.Find("LabelAnswer/textAswer").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI _alertIndex = newItem.transform.Find("textList").GetComponent<TextMeshProUGUI>();
+
+        _alertQuiz.text = quiz;
+        _alertAnswer.text = answer.ToString();
+        _alertIndex.text = index.ToString();
+
+        Color color = Color.green;  
+        if (ColorUtility.TryParseHtmlString(colors, out color))
+        {
+            _background.color = color;
         }
     }
     IEnumerator showAlertMessage()
